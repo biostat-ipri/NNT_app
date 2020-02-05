6aefbe85e9f11ab4c68cc0116382e38c83a95bf9
Cecile-P <cecile.pizot@i-pri.org>
2020-02-05 13:04:02 +0100

    Customisation 1

--- R/Shiny_NNT.R
+++ R/Shiny_NNT.R
@@ -10,51 +10,90 @@ library(devtools)
 library(NNT)
 
 ui <- fluidPage(
-  numericInput(inputId = "cases1", 
-            label = "Enter the number of cases in the first category of exposure",
-            value = ""),
-  numericInput(inputId = "py1", 
-            label = "Enter the number of PY in the first category of exposure",
-            value = ""),
-  numericInput(inputId = "cases2", 
-            label = "Enter the number of cases in the second category of exposure",
-            value = ""),
-  numericInput(inputId = "py2", 
-            label = "Enter the number of PY in the first category of exposure",
-            value = ""),
-  actionButton(inputId = "button_compute", 
-               label = "Compute NNT"),
+  
+  fluidRow(
+    column(4),
+    column(5,tags$h1("NNT computation"))
+          ),
+  
+  tags$p("This app allows you to easily compute the number needed to treat."),
+ 
+  tags$br(),
+  
+  tags$head(
+    tags$style(HTML(
+      "label {font-weight:normal;}"
+    ))
+  ),
+  
+  fluidRow(
+    column(3,tags$strong("First exposure quantile")),
+    column(4,numericInput(inputId = "cases1", 
+                        label = "Enter the number of cases observed in the first exposure quantile",
+                        value = "")),
+    column(4,numericInput(inputId = "py1", 
+                          label = "Enter the number of PY in the first exposure quantile",
+                          value = ""))
+          ),
+  
+  fluidRow(
+    column(3,tags$strong("Second exposure quantile")),
+    column(4,numericInput(inputId = "cases2", 
+                          label = "Enter the number of cases observed in the second exposure quantile",
+                          value = "")),
+    column(4,numericInput(inputId = "py1", 
+                          label = "Enter the number of PY in the second exposure quantile",
+                          value = ""))
+          ),
+  
+  tags$br(),
+  
+  fluidRow(
+    column(5),
+    column(2,actionButton(inputId = "button_compute", 
+               label = "Compute NNT"))
+          ),
+  
+  tags$br(),
+  tags$br(),
+  
   textOutput("NNT"),
-  textOutput("ARR")
+  
+  textOutput("ARR"),
+  
+  textOutput("Error")
+  
   # plotOutput("plot")
 )
 
 server <- function(input, output) {
   
-    # 
-  # observeEvent(input$button_compute, {
-    NNT <- eventReactive(input$button_compute,{
-      compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),0.05)[["NNT"]]
-    })
-    NNT_CI <- eventReactive(input$button_compute,{
-      compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),0.05)[["CI_NNT"]]
-    })
-    ARR <- eventReactive(input$button_compute,{
-      compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),0.05)[["ARR"]]
-    })
-    ARR_CI <- eventReactive(input$button_compute,{
-      compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),0.05)[["CI_ARR"]]
-    })
-    
+  NNT_computation <- eventReactive(input$button_compute,{
+    compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),0.05)
+  })
+  # if(NNT_computation()[["ARR"]]<0){
+  #   output$Error <- renderText({
+  #   "Warning: The ARR is negative, check your data"
+  # })
+  # }else{
     output$ARR <- renderText({
-      paste("The absolute risk reduction is:",round(ARR(),2),"with 95%CI",paste("[",paste(round(ARR_CI(),2),collapse="; "),"]"),sep=" ")
+      ARR <- NNT_computation()[["ARR"]]
+      ARR_CI <- NNT_computation()[["CI_ARR"]]
+      paste("The absolute risk reduction is:",format(round(ARR,2),nsmall=2),"with 95%CI",paste("[",paste(format(round(ARR_CI,2),nsmall=2),collapse="; "),"]"),sep=" ")
+    })
+  
+    output$NNT <- renderText({
+      NNT <- NNT_computation()[["NNT"]]
+      NNT_CI <- NNT_computation()[["CI_NNT"]]
+      paste("The number needed to treat is:",round(NNT),"with 95%CI",paste("[",paste(round(NNT_CI),collapse="; "),"]"),sep=" ")
     })
     
-    output$NNT <- renderText({
-      paste("The number needed to treat is:",round(NNT()),"with 95%CI",paste("[",paste(round(NNT_CI()),collapse="; "),"]"),sep=" ")
+    output$Error <- renderText({
+      if(NNT_computation()[["ARR"]]<0){"Warning: The ARR is negative, check your data!"}
+      else{""}
     })
- 
- # })
+  # }
+
  
   # output$plot <- renderPlot({
   #   title <- "NNT with CI"
