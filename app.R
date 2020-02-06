148483f61c8eb42c6cb277974341a862af133443
Cecile-P <cecile.pizot@i-pri.org>
2020-02-06 17:25:59 +0100

    customisation3 et ajout d'un champ alpha

--- app.R
+++ app.R
@@ -13,20 +13,20 @@ ui <- fluidPage(
   
   fluidRow(
     column(4),
-    column(5,tags$h3("Confidence intervals for the number needed to treat"))
+    column(6,tags$h1("Number needed to treat"))
           ),
   
-  tags$h5("This app provide a graphical interface to the R package NNT",
+  tags$h5("This app provides a graphical interface to the R package NNT",
   tags$a(href= "https://github.com/gilles-guillot/NNT",tags$u("(github.com/gilles-guillot/NNT)."))),
-  tags$h5("It computes the number needed to treat and a confidence interval."),
+  tags$h5("It computes the number needed to treat and the corresponding confidence interval."),
   tags$h5("It was developed at", 
-          tags$a(href="https://i-pri.org/",tags$u("iPRI")),"in the context of the article \"Efforts needed for preventing breast and colorectal cancer through changes in dietary patterns\"
-          , Mullie P., G. Guillot, C. Pizot, Ph. Autier, Peter Boyle."),
+          tags$a(href="https://i-pri.org/",tags$u("iPRI")),
+          "in the context of the article \"Efforts needed for preventing breast and colorectal cancer through changes in dietary patterns\"
+          , P. Mullie, G. Guillot, C. Pizot, P. Autier, P. Boyle."),
  
   tags$br(),
   tags$br(),
-  tags$br(),
-  
+
   tags$head(
     tags$style(HTML(
       "label {font-weight:normal;}"
@@ -51,10 +51,19 @@ ui <- fluidPage(
                           label = "Enter the number of cases observed in the second exposure group",
                           value = "",
                           min=0)),
-    column(4,numericInput(inputId = "py1", 
+    column(4,numericInput(inputId = "py2", 
                           label = "Enter the number of PY in the second exposure group",
                           value = "",
                           min=1))
+          ),
+  
+  fluidRow(
+    column(3),
+    column(3,numericInput(inputId = "alpha", 
+                        label = "Enter your alpha error (in %)",
+                        value = "5",
+                        min=0,
+                        max=100))
           ),
   
   tags$br(),
@@ -82,24 +91,34 @@ ui <- fluidPage(
 server <- function(input, output) {
 
   NNT_computation <- eventReactive(input$button_compute,{
-      compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),0.05)
+    torf <- NA %in% c(input$cases1,input$cases2,input$py1,input$py2,input$alpha)
+    if(!torf){
+      compute_NNT(c(input$cases1,input$cases2),c(input$py1,input$py2),input$alpha/100)
+    }else{
+      list("ARR"=NA,CI_ARR=c(NA,NA),"NNT"=c(NA),"CI_NNT"=c(NA,NA))
+    }
   })
-
+  
   output$ARR <- renderText({
     ARR <- NNT_computation()[["ARR"]]
     ARR_CI <- NNT_computation()[["CI_ARR"]]
-    paste("The absolute risk reduction is:",format(round(ARR,5),nsmall=5),"with 95%CI",paste("[",paste(format(round(ARR_CI,5),nsmall=5),collapse="; "),"]"),sep=" ")
+    if(!is.na(ARR) & ARR>0){
+      paste("The absolute risk reduction is: ",format(round(ARR,5),nsmall=5)," with ",100-input$alpha,"%CI ",paste("[",paste(format(round(ARR_CI,5),nsmall=5),collapse="; "),"]"),sep="")
+    }
   })
   
   output$NNT <- renderText({
+    ARR <- NNT_computation()[["ARR"]]
     NNT <- NNT_computation()[["NNT"]]
     NNT_CI <- NNT_computation()[["CI_NNT"]]
-    paste("The number needed to treat is:",round(NNT),"with 95%CI",paste("[",paste(round(NNT_CI),collapse="; "),"]"),sep=" ")
+    if(!is.na(NNT) & ARR>0){
+      paste("The number needed to treat is: ",round(NNT)," with ",100-input$alpha,"%CI ",paste("[",paste(round(NNT_CI),collapse="; "),"]"),sep="")
+    }
   })
-    
+  #
   output$Error <- renderText({
-    if(NNT_computation()[["ARR"]]<0){"Warning: The ARR is negative, check your data!"}
-    else{""}
+    if(is.na(NNT_computation()[["ARR"]])){"Warning: Some values are missing!"}  
+    else if(NNT_computation()[["ARR"]]<=0){"Warning: The absolute risk reduction is negative or null, check your data!"}
   })
   
 
